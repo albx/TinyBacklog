82c40c30d01095518b15013c3a72b476a8fa2e85
Alberto Mori <albi.mo87@hotmail.it>
2021-11-01 11:32:37 +0100

    Add authentication

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,12 +1,9 @@
 using System;
 using System.Net.Http;
-using System.Collections.Generic;
 using System.Threading.Tasks;
-using System.Text;
+using AzureStaticWebApps.Blazor.Authentication;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
 
 namespace TinyBacklog.Web.App
 {
@@ -17,9 +14,10 @@ namespace TinyBacklog.Web.App
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("#app");
 
-            var baseAddress = builder.Configuration["BaseAddress"] ?? builder.HostEnvironment.BaseAddress;
+            var baseAddress = builder.HostEnvironment.BaseAddress;
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(baseAddress) });
+            builder.Services.AddStaticWebAppsAuthentication();
 
             await builder.Build().RunAsync();
         }
